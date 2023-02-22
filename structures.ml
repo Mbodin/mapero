2d07e8a5ba353147aeadc189a87e4e48e6177a74
Martin Constantino–Bodin <martin.bodin@ens-lyon.org>
2023-02-22 23:08:45 +0100

    A more general implementation of priorities.

--- structures.ml
+++ structures.ml
@@ -1,30 +1,50 @@
 
-open StructuresSig
+include StructuresSig
 
 
 type priority =
+  | VeryLow
   | Low
   | Medium
   | High
+  | VeryHigh
 
-let priority_lt p1 p2 =
-  match p1, p2 with
-  | Low, (Medium | High) -> true
-  | Medium, High -> true
-  | _, _ -> false
+module Priority = struct
+
+  let to_number = function
+    | VeryLow -> -2
+    | Low -> -1
+    | Medium -> 0
+    | High -> 1
+    | VeryHigh -> 2
+
+  let compare p1 p2 = to_number p1 - to_number p2
+
+end
 
 
-module addPriority (S : DirectedSet) = struct
+module AddPriority (P : Set.OrderedType) (S : DirectedSet) = struct
 
   type t = S.t * priority
 
   let join (e1, p1) (e2, p2) =
-    if priority_lt p1 p2 then (e2, p2)
-    else if priority_lt p2 p1 then (e1, p1)
-    else (
-      assert (p1 = p2) ;
-      (S.join e1 e2, p1)
-    )
+    let c = P.compare p1 p2 in
+    if c < 0 then (e2, p2)
+    else if c > 0 then (e1, p1)
+    else (* c = 0 *) (S.join e1 e2, p1)
+
+end
+
+
+module DirectedSetFromOrder (S : Set.OrderedType) = struct
+
+  include S
+
+  let join e1 e2 =
+    let c = P.compare e1 e2 in
+    if c < 0 then e2
+    else if c > 0 then e1
+    else (* c = 0 *) e1
 
 end
 
--- structures.mli
+++ structures.mli
@@ -1,15 +1,20 @@
 
-open StructuresSig
+include StructuresSig
 
 
 type priority =
+  | VeryLow
   | Low
   | Medium
   | High
+  | VeryHigh
 
-val priority_lt : priority -> priority -> bool
+module Priority : Set.OrderedType with type t = priority
 
 
-module addPriority (S : DirectedSet)
-  : DirectedSet with type t = S.t * priority
+(* Add a type of priorities to a given directed set: higher priorities always take precedence. *)
+module AddPriority (P : Set.OrderedType) (S : DirectedSet)
+  : DirectedSet with type t = S.t * P.t
+
+module DirectedSetFromOrder (S : Set.OrderedType) : DirectedSet
 
