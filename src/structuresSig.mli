47cb32226cdb369cbe4c25fae544fc528c794d5f
Martin Constantino–Bodin <martin.bodin@ens-lyon.org>
2023-02-28 23:32:10 +0100

    Compilation files.

--- src/structuresSig.mli
+++ src/structuresSig.mli
@@ -3,7 +3,7 @@
   all kinds of information.  We could do it with a monoid, but I feel that directed sets
   provide a better way to think these kinds of relations. *)
 
-module type DirectedSet = begin
+module type DirectedSet = sig
 
   type t
 
