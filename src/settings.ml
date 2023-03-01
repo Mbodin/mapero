1325670104345d6a01f4318e49ae2578248d1e39
Martin Bodin <martin.bodin@inria.fr>
2023-03-01 23:04:23 +0100

    Reorganising the settings.

--- src/settings.ml
+++ src/settings.ml
@@ -2,50 +2,67 @@
 let exact l = List.map (fun (k, v) -> (Overpass.Exact k, Overpass.Exact v)) l
 let regexp l = List.map (fun (k, v) -> (Overpass.Regexp k, Overpass.Regexp v)) l
 let vregexp l = List.map (fun (k, v) -> (Overpass.Exact k, Overpass.Regexp v)) l
-let expand l =
-  List.concat_map (fun (k, vs) -> List.map (fun v -> (Overpass.Exact k, Overpass.Exact v)) vs) l
+let rec expand cxt = function
+  | [] -> cxt []
+  | (k, vs) :: l ->
+    List.concat_map (fun v -> expand (fun l -> cxt ((Overpass.Exact k, Overpass.Exact v) :: l)) l) vs
 
 let node l ?(style = Map.Round) color =
-  (Overpass.Node, exact l, style, color)
+  [(Overpass.Node, exact l, style, color)]
 
 let rnode l ?(style = Map.Round) color =
-  (Overpass.Node, regexp l, style, color)
+  [(Overpass.Node, regexp l, style, color)]
 
 let rvnode l ?(style = Map.Round) color =
-  (Overpass.Node, vregexp l, style, color)
+  [(Overpass.Node, vregexp l, style, color)]
 
 let enode l ?(style = Map.Round) color =
-  (Overpass.Node, expand l, style, color)
+  expand (fun l -> [(Overpass.Node, l, style, color)]) l
 
 let way l ?(style = Map.Adaptive) color =
-  (Overpass.Way, exact l, style, color)
+  [(Overpass.Way, exact l, style, color)]
 
 let rway l ?(style = Map.Adaptive) color =
-  (Overpass.Way, regexp l, style, color)
+  [(Overpass.Way, regexp l, style, color)]
 
 let vrway l ?(style = Map.Adaptive) color =
-  (Overpass.Way, vregexp l, style, color)
+  [(Overpass.Way, vregexp l, style, color)]
 
 let eway l ?(style = Map.Adaptive) color =
-  (Overpass.Way, expand l, style, color)
+  expand (fun l -> [(Overpass.Way, l, style, color)]) l
+
+let poly l ?(style = Map.Adaptive) color =
+  [(Overpass.Polygon, exact l, style, color)]
+
+let rpoly l ?(style = Map.Adaptive) color =
+  [(Overpass.Polygon, regexp l, style, color)]
+
+let vrpoly l ?(style = Map.Adaptive) color =
+  [(Overpass.Polygon, vregexp l, style, color)]
+
+let epoly l ?(style = Map.Adaptive) color =
+  expand (fun l -> [(Overpass.Polygon, l, style, color)]) l
 
 
 
-let objects = [
-  (* PoIs *)
-  node [("natural", "tree")] Dot.Bright_green ;
-  node [("leisure", "playground")] Dot.Satin_trans_clear ;
-  way [("leisure", "playground")] Dot.Satin_trans_clear ;
-  node [("amenity", "public_bookcase")] Dot.Trans_orange ;
-  node [("emergency", "fire_hydrant"); ("fire_hydrant:type", "pillar")] Dot.Coral ;
-  node [("amenity", "bicycle_parking"); ("bicycle_parking", "wide_stands")] Dot.Dark_turquoise ;
-  node [("amenity", "bicycle_parking"); ("bicycle_parking", "stands")] Dot.Dark_turquoise ;
-  node [("amenity", "bicycle_parking"); ("bicycle_parking", "bollard")] Dot.Dark_turquoise ;
-  (* Ways *)
-  (* TODO: expand doesn't actually work like that: this will be looking for the intersection of all the values (which is empty) instead of the sum. *)
-  eway [("highway", ["motorway"; "trunk"; "primary"; "secondary"; "tertiary"; "unclassified"; "road"])] Dot.Dark_azure ;
-  eway [("highway", ["motorway_link"; "trunk_link"; "primary_link"; "secondary_link"; "tertiary_link"])] Dot.Dark_azure ;
-  eway [("highway", ["residential"; "living_street"; "service"; "track"])] Dot.Light_aqua ;
-  eway [("highway", ["pedestrian"; "footway"; "bridleway"; "path"; "cycleway"; "crossing"])] Dot.Bright_light_blue ;
-]
+let objects =
+  List.flatten [
+    (* Main PoIs I'm interested at *)
+    node [("natural", "tree")] Dot.Bright_green ;
+    node [("leisure", "playground")] Dot.Satin_trans_clear ;
+    way [("leisure", "playground")] Dot.Satin_trans_clear ;
+    node [("amenity", "public_bookcase")] Dot.Trans_orange ;
+    (* Some good references PoIs *)
+    node [("amenity", "bicycle_parking"); ("bicycle_parking", "wide_stands")] Dot.Dark_turquoise ;
+    node [("amenity", "bicycle_parking"); ("bicycle_parking", "stands")] Dot.Dark_turquoise ;
+    node [("amenity", "bicycle_parking"); ("bicycle_parking", "bollard")] Dot.Dark_turquoise ;
+    node [("emergency", "fire_hydrant"); ("fire_hydrant:type", "pillar")] Dot.Coral ;
+    (* Structurung elements *)
+    vrway [("waterway", ".*")] Dot.Dark_azure ;
+    eway [("highway", ["motorway"; "trunk"; "primary"; "secondary"; "tertiary"; "unclassified"; "road"])] Dot.Lavender ;
+    eway [("highway", ["motorway_link"; "trunk_link"; "primary_link"; "secondary_link"; "tertiary_link"])] Dot.Lavender ;
+    eway [("highway", ["residential"; "living_street"; "service"; "track"])] Dot.Yellow ;
+    eway [("highway", ["pedestrian"; "footway"; "bridleway"; "path"; "cycleway"; "crossing"])] Dot.Bright_light_blue ;
+    vrpoly [("building", ".*")] Dot.Bright_pink
+  ]
 
